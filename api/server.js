669550f92860aa1c5b431ba55def3a0a19b75fc5
Emre Taş <75886362+emretasss@users.noreply.github.com>
2023-12-08 04:40:43 +0300

    env and server mongo create

--- api/server.js
+++ api/server.js
@@ -1,4 +1,62 @@
- 
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
- 
- 
+import { configDotenv } from 'dotenv';
+
+import express from 'express';
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+configDotenv();
+const uri = process.env.MONGO;
+
+
+
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+
+const app =express();
+app.listen
+( 8800, ()=>{
+    run();
+    console.log("Server is running on port 3000");
+    });
+
+
+
+
+
+
+
