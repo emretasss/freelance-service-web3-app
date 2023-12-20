47a4464ecfa94e41c546e2e9ca78575432643081
Emre Taş <75886362+emretasss@users.noreply.github.com>
2023-12-21 01:10:39 +0300

    auth contreller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,28 +1,60 @@
-export const register= async (req,res) => {
-    try{
-        const newUser =new User({
-            username:"test",
-            email:"test",
-            password:"test",
-            country:"test",
-            
-        })
-        
-        await newUser.save();
-        res.status(201).send("user has been created");
-   
+import User from "../models/user.model.js";
+import createError from "../utils/createError.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+export const register = async (req, res, next) => {
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 5);
+    const newUser = new User({
+        username: "test",
+        email: " test",
+        password: "test",
+      password: "test",
+    });
 
+    await newUser.save();
+    res.status(201).send("User has been created.");
+  } catch (err) {
+    next(err);
+  }
+};
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
 
-    }
-    catch(err){
-        res.status(500).send("something went wrong");
-        }
+    if (!user) return next(createError(404, "User not found!"));
 
-}
-export const login= (req,res) => {
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect)
+      return next(createError(400, "Wrong password or username!"));
 
-}
-export const logout= (req,res) => {
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
 
-}
+    const { password, ...info } = user._doc;
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const logout = async (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .send("User has been logged out.");
+};
