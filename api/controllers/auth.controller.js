0d6d0d75d2284be45da67e42f3c73f8010cf4ac1
Emre Taş <75886362+emretasss@users.noreply.github.com>
2023-12-22 02:38:06 +0300

    token_cookie

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -30,7 +30,9 @@ export const login = async (req, res, next) => {
 
     const { password, ...info } = user._doc;
     res
-      
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
       .status(200)
       .send(info);
   } catch (err) {
