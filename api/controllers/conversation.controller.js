b365504164629b744044b4a65021247f8f08fdfe
Emre Taş <75886362+emretasss@users.noreply.github.com>
2023-12-21 03:51:29 +0300

    getconversations

--- api/controllers/conversation.controller.js
+++ api/controllers/conversation.controller.js
@@ -39,16 +39,15 @@ export const updateConversation = async (req, res, next) => {
 };
 
 export const getSingleConversation = async (req, res, next) => {
- 
-};
-
-export const getConversations = async (req, res, next) => {
   try {
-    const conversations = await Conversation.find(
-      req.isSeller ? { sellerId: req.userId } : { buyerId: req.userId }
-    ).sort({ updatedAt: -1 });
-    res.status(200).send(conversations);
+    const conversation = await Conversation.findOne({ id: req.params.id });
+    if (!conversation) return next(createError(404, "Not found!"));
+    res.status(200).send(conversation);
   } catch (err) {
     next(err);
   }
+};
+
+export const getConversations = async (req, res, next) => {
+  
 };
