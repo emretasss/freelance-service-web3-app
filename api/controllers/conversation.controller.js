a9f2d0de595f30ae43be5ce43ca74eb25a78abe0
Emre Taş <75886362+emretasss@users.noreply.github.com>
2023-12-21 03:49:40 +0300

    createConversation new

--- api/controllers/conversation.controller.js
+++ api/controllers/conversation.controller.js
@@ -2,20 +2,7 @@ import createError from "../utils/createError.js";
 import Conversation from "../models/conversation.model.js";
 
 export const createConversation = async (req, res, next) => {
-  const newConversation = new Conversation({
-    id: req.isSeller ? req.userId + req.body.to : req.body.to + req.userId,
-    sellerId: req.isSeller ? req.userId : req.body.to,
-    buyerId: req.isSeller ? req.body.to : req.userId,
-    readBySeller: req.isSeller,
-    readByBuyer: !req.isSeller,
-  });
-
-  try {
-    const savedConversation = await newConversation.save();
-    res.status(201).send(savedConversation);
-  } catch (err) {
-    next(err);
-  }
+ 
 };
 
 export const updateConversation = async (req, res, next) => {
