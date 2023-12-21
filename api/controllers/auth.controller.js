a9c4220bd0ba8f3c4c4eaafb59e60ee615d56633
Emre Taş <75886362+emretasss@users.noreply.github.com>
2023-12-21 03:58:42 +0300

    utilseror

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import createError from "../utils/createError.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+
+
 
 export const register = async (req, res, next) => {
   try {
