710d533e172dc30b69e4a92d74a13528b28539fe
Emre Taş <75886362+emretasss@users.noreply.github.com>
2023-12-21 03:59:37 +0300

    install update  bcrypt

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import createError from "../utils/createError.js";
-
+import bcrypt from "bcrypt";
 
 
 export const register = async (req, res, next) => {
