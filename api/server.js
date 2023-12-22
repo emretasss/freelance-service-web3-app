281769b80723c09250951233783138c7abc9323c
Emre Taş <75886362+emretasss@users.noreply.github.com>
2023-12-22 05:09:05 +0300

    error status testing

--- api/server.js
+++ api/server.js
@@ -23,6 +23,12 @@ const connect = async () => {
 };
 
 
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+
+  return res.status(errorStatus).send(errorMessage);
+});
 
 
 
