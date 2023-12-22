975dc55f73e0848d59839bdadd475fab15eb7d25
Emre Taş <75886362+emretasss@users.noreply.github.com>
2023-12-22 03:48:55 +0300

    jwt sign athntcdd

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -5,7 +5,9 @@ export const deleteUser = async (req, res) => {
 
   if (!token) res.status(401).send("you are not authentaticated")
 
-  
+  jwt.verify(token,process.env.JWT_KEY,(err,payload)=>{
+  }
+  )
   await User.findByIdAndDelete(req.params.id);
   res.status(200).send("deleted.");
 
