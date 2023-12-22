7f68debda74ca0d3784190def095c5e654cd0784
Emre Taş <75886362+emretasss@users.noreply.github.com>
2023-12-22 03:40:58 +0300

    delete user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,17 +1,5 @@
 import User from "../models/user.model.js";
-import createError from "../utils/createError.js";
+import jwt from "jsonwebtoken";
 
-export const deleteUser = async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-
-  if (req.userId !== user._id.toString()) {
-    return next(createError(403, "You can delete only your account!"));
-  }
-  await User.findByIdAndDelete(req.params.id);
-  res.status(200).send("deleted.");
-};
-export const getUser = async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-
-  res.status(200).send(user);
-};
+ export const deleteUser = async (req,res) => {
+ }
