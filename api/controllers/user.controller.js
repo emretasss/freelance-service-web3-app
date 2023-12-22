74afb0d6eef25769a502f373229684e71524dc79
Emre Taş <75886362+emretasss@users.noreply.github.com>
2023-12-22 03:47:13 +0300

    jwt sign athntcdd

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,16 +1,12 @@
 import User from "../models/user.model.js";
 import jwt  from "jsonwebtoken"
 
-export const deleteUser = async (req, res, next) => {
-  const user = await User.findById(req.params.id);
+export const deleteUser = async (req, res) => {
 
   if (!token) res.status(401).send("you are not authentaticated")
+
+  
   await User.findByIdAndDelete(req.params.id);
   res.status(200).send("deleted.");
 
-};
-export const getUser = async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-
-  res.status(200).send(user);
 };
